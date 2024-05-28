13f108f1a11b9162b0e6c4334e5f29450849caa2
Wanderson Junior <devwjota021@gmail.com>
2024-05-28 15:12:37 -0300

    V2

--- Exercicios/Desafio usuario e senha.py
+++ Exercicios/Desafio usuario e senha.py
@@ -4,7 +4,7 @@ from time import sleep
 # Pedir informação
 
 # Email
-email = pyautogui.prompt(text='Digite seu Email😁',title=
+email = pyautogui.prompt(text='Digite seu Email 😁',title=
 'Digite seu Login e Senha')
 print(f'voçe digitou {email}')
 pyautogui.moveTo(807,121,duration=2)
@@ -12,7 +12,7 @@ pyautogui.click(button='left')
 pyautogui.typewrite(f'{email}')
 pyautogui.hotkey('ctrl','v')
 # Senha
-senha = pyautogui.password(text='digite sua senha:',title='Digite sua Senha🐱‍👤',mask='*')
+senha = pyautogui.password(text='digite sua senha:',title='Digite sua Senha 🐱‍👤',mask='*')
 print(senha)
 pyautogui.hotkey('enter')
 pyautogui.typewrite(senha)
